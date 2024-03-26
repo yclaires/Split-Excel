e250e960f6b3ae44b0fc49cea47995242c176b25
Yue <NA>
2024-03-26 12:03:24 +0100

    update code using pathlib instead of os

--- split_excel_by_column.py
+++ split_excel_by_column.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from pathlib import Path
 
 # Read the Excel file
 file_path = 'Excel-Split.xlsx'  # Replace 'input_file.xlsx' with your Excel file path
@@ -10,8 +11,8 @@ split_column = 'Kontinent'  # Replace 'Column_Name' with the name of the column 
 unique_values = df['Kontinent'].unique()
 
 # Create a target directory to save the split Excel files
-target_directory = 'output_folder'  # Replace 'output_folder' with the desired target directory name
-os.makedirs(target_directory, exist_ok=True)
+target_directory = Path.cwd() / 'output_folder'  # Replace 'output_folder' with the desired target directory name
+#os.makedirs(target_directory, exist_ok=True) # Use os.makedirs function to create the target directory
 
 # Split the data based on unique values in the specified column
 for value in unique_values:
@@ -19,7 +20,9 @@ for value in unique_values:
     subset = df[df['Kontinent'] == value]
     
     # Define the output file path
-    output_file_path = os.path.join(target_directory, f'{value}.xlsx')
+    #output_file_path = os.path.join(target_directory, f'{value}.xlsx')  # Use 'os.path.join()' function to define the output file
+    output_file_path = Path(target_directory) / f'{value}.xlsx'  # Use ' Path() / ' to define the output file
+
     
     # Write the subset to a new Excel file
     subset.to_excel(output_file_path, index=False)
